bb8be8a6bb6f4d7cfa965f320ad46751ba083f06
KONATE Bakary <b0konate@enib.fr>
2022-03-15 14:07:42 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Route, Switch } from 'react-router-dom'
 import AllMeetups from './pages/AllMeetups';
 import NewMeetup from './pages/NewMeetup';
 import Favorites from './pages/Favorites';
-import MainNavigation from './components/layout/MainNavigation';
 import Layout from './components/layout/Layout'
 function App() {
 
--- src/components/meetups/MeetupList.js
+++ src/components/meetups/MeetupList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import classes from './MeetupList.module.css';
 import MeetupItem from './MeetupItem';
 function MeetupList(props) {
